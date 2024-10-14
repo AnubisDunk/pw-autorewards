8ce4bf71c19160026694974665394cd9a0190a19
Aleksandrs <alex.bar.dev@gmail.com>
2024-10-14 13:04:24 +0300

    search functionality

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -18,22 +18,36 @@ test('has title', async ({ page }) => {
   await page.locator('#declineButton').click();
   await expect(page).toHaveTitle("Microsoft Rewards");
 
-  await expect(page.locator('.c-card-content a').first()).toBeVisible();
+  //await expect(page.locator('.c-card-content a').first()).toBeVisible();
   const rewards = page.locator('.c-card-content a');
 
-  for (let i = 0; i < await rewards.count(); i++) {
+  for (let i = 0; i < 2; i++) {
     await rewards.nth(i).click();
     //console.log("Reward");
   }
 
 });
 
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
+test('execute 30 searches', async ({ page }) => {
+  test.slow();
+  await page.goto('https://bing.com/');
+  await page.locator('#bnp_btn_accept').click();
+  const search = page.locator('#sb_form_q');
+  const loginField = page.getByTestId('i0116');
+  const passField = page.getByTestId('i0118');
+  await page.locator('#id_l').click();
+  await page.locator('#b_idProviders li a').first().click();
+  await loginField.fill("soldermain69@outlook.com");
+  await loginField.press('Enter');
+  await passField.fill("fervexSOLDER");
+  await passField.press('Enter');
+  await expect(page).toHaveTitle("Stay signed in?");
+  await page.locator('#declineButton').click();
 
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
+   for (let i = 0; i < 29; i++) {
+    await search.fill(`TestNR:${i}`);
+    await search.press('Enter');
+  }
 
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
+
+});
